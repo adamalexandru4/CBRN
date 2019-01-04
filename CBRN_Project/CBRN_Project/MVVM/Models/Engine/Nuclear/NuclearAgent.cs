579ae5264f5d4c5faebdc4752e9d59d8b0915ca2
DomideAdrian <domideadrian@gmail.com>
2019-01-04 15:26:26 +0200

    Complete Nuclear

--- CBRN_Project/CBRN_Project/MVVM/Models/Engine/Nuclear/NuclearAgent.cs
+++ CBRN_Project/CBRN_Project/MVVM/Models/Engine/Nuclear/NuclearAgent.cs
@@ -11,7 +11,6 @@ namespace CBRN_Project.MVVM.Models.Engine.Nuclear
     class NuclearAgent
     {
         #region Properties
-        private static NuclearChallenge NucChallenge { get; set; }
         private static NuclearProperties NucProperties { get; set; }
 
         //Tables
@@ -24,8 +23,8 @@ namespace CBRN_Project.MVVM.Models.Engine.Nuclear
         private static void CalculateNucChallenge(Icon icon)
         {
             Init(icon);
+            GenerateOutputTable();
             CalculateChallenge(icon);
-            GenerateOutput();
         }
 
         public static void Init(Icon icon)
@@ -42,10 +41,15 @@ namespace CBRN_Project.MVVM.Models.Engine.Nuclear
         public static void CalculateChallenge(Icon icon)
         {
             CalculateRange();
-            double procent = CalculateThermalChallengedPersonsNumber(icon);
+            
+            List <string> wiaBlast = CalculateBlastWIA(NucProperties.RangeBl);
+            List <string> wiaWb = CalculateWBWIA(NucProperties.RangeWB, true);
+            List <string> wiaThermal = CalculateThermalWIA(NucProperties.RangeTh);
+
+            InterpretState(wiaWb, wiaBlast, wiaThermal, icon);
         }
 
-        public static void GenerateOutput()
+        public static void GenerateOutputTable()
         {
             CreateTable();
         }
@@ -122,7 +126,7 @@ namespace CBRN_Project.MVVM.Models.Engine.Nuclear
                 return 1;
             if (TestRange(10, 15, dose))
                 return 2;
-            if (TestRange(15, 20, dose))\
+            if (TestRange(15, 20, dose))
                 return 3;
             if (TestRange(20, 30, dose))
                 return 4;
@@ -147,7 +151,185 @@ namespace CBRN_Project.MVVM.Models.Engine.Nuclear
             return result;
         }
 
+        private static List<string> CalculateBlastWIA(int range)
+        {
+            List<string> result = new List<string>();
+            if(range == 1)
+                return new List<string> { "WIA", "RTD", "9", "100"};
+            if (range == 2)
+                return new List<string> { "WIA", "RTD", "22", "100"};
+            if (range == 3)
+                return new List<string> { "WIA", "RTD", "30+", "100"};
+            if (range == 4)
+                return new List<string> { "WIA", "DOW", "2", "23", "CONV", "30+", "77"};
+            else
+                return new List<string> { "None" };
+                
+        }
+
+        private static List<string> CalculateThermalWIA(int range)
+        {
+            List<string> result = new List<string>();
+            if (range == 1 || range == 2)
+                return new List<string> { "WIA", "RTD", "22", "100" };
+            if (range == 3 || range == 4)
+                return new List<string> { "WIA", "CONV", "30+", "50", "RTD", "30+", "50" };
+            if (range == 5)
+                return new List<string> { "WIA", "DOW", "9", "25", "CONV", "30+", "75" };
+            if (range == 6)
+                return new List<string> { "WIA", "DOW", "7", "75", "CONV", "30+", "25" };
+            else
+                return new List<string> { "None" };
+        }
+
+        private static List<string> CalculateWBWIA(int range, bool GCSF)
+        {
+            List<string> result = new List<string>();
+            if(range == 0)
+                return new List<string> { "None" };
+            if (range == 1)
+                return new List<string> { "WIA", "CONV", "2", "100" };
+            if (GCSF)
+            { 
+                if (range == 2 || range == 3 || range == 4)
+                    return new List<string> { "WIA", "CONV", "30", "100" };
+                else
+                    return new List<string> { "DOW"};
+            }
+            else
+            {
+                if (range == 2 || range == 3)
+                    return new List<string> { "WIA", "CONV", "30", "100" };
+                else
+                    return new List<string> { "DOW" };
+            }
+        }
+
+        private static void InterpretState(List<string> wbstat, List<string> blstat, List<string> thstat, Icon icon)
+        {
+            bool WB = true, Bl = true, Th = true;
+            double procent = 1;
+            if (wbstat.Contains("None") == true)
+                WB = false;
+            if (blstat.Contains("None") == true)
+                Bl = false;
+            if (thstat.Contains("None") == true)
+                Th = false;
+            if (WB == false && Bl == false && Th == false)
+                return;
+
+            if (Th == true)
+                procent = CalculateThermalChallengedPersonsNumber(icon);
+
+            List<int> newDOW = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            List<int> newWIA = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            List<int> newCONV = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            List<int> newRTD = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            List<int> newKIA = new List<int>() { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            if (NucProperties.NucBlast > getThresholdBlast(10)) //need o yield default 10 KT
+            {
+                newKIA[0] += Convert.ToInt32(icon.Personnel);
+                return;
+            }
+
+            if(wbstat.Contains("DOW"))
+            {
+                double time = 429 * Math.Pow(NucProperties.NucWholeBody, -1.3);
+                int day = Convert.ToInt32(Math.Round(time));
+                if (day > 1)
+                    newWIA[0] += (int)icon.Personnel;
+                newDOW[ConvertToTable(day)] += (int)icon.Personnel;
+                return;
+            }
+            else
+            {
+                newWIA[0] += (int)icon.Personnel;
+                if(thstat[1] == "DOW")
+                {
+                    int day = ConvertToTable(Convert.ToInt32(thstat[2]));
+                    newDOW[day] += Convert.ToInt32(icon.Personnel * procent * Convert.ToInt32(thstat[3]) /100);
+                    newCONV[8] += Convert.ToInt32(icon.Personnel * procent * Convert.ToInt32(thstat[6]) / 100);
+                    return;
+                }
+
+                if(blstat[1] == "DOW")
+                {
+                    newWIA[0] += (int)icon.Personnel;
+                    newDOW[2] += (int)(icon.Personnel * 0.23);
+                    newCONV[9] += (int)(icon.Personnel * 0.77);
+                    return;
+                }
+
+                if(wbstat[1] == "CONV" || thstat[1] == "CONV" )
+                {
+                    int day = Math.Max(Convert.ToInt32(wbstat[2]),Convert.ToInt32(thstat[2]));
+                    newCONV[ConvertToTable(day)] += Convert.ToInt32(icon.Personnel);
+                    return;
+                }
+
+                if(thstat[1] == "RTD" && blstat[1] == "RTD")
+                {
+                    int day = Math.Max(Convert.ToInt32(thstat[2]), Convert.ToInt32(blstat[2]));
+                    newRTD[ConvertToTable(day)] += Convert.ToInt32(icon.Personnel);
+                    return;
+                }
+
+                if (thstat[1] == "RTD")
+                {
+                    int day = Convert.ToInt32(thstat[2]);
+                    newRTD[ConvertToTable(day)] += Convert.ToInt32(icon.Personnel);
+                    return;
+                }
+
+                if (blstat[1] == "RTD")
+                {
+                    int day = Convert.ToInt32(blstat[2]);
+                    newRTD[ConvertToTable(day)] += Convert.ToInt32(icon.Personnel);
+                    return;
+                }
+            }
+            CompletTable(newKIA, newDOW, newWIA, newCONV, newRTD);
+        }
+
+        private static void CompletTable(List<int> newKIA, List<int> newDOW, List<int> newWIA, List<int> newCONV, List<int> newRTD )
+        {
+            for(int i = 0; i < newKIA.Count(); i++)
+            {
+                OutputTable.Rows[0].SetField(i, OutputTable.Rows[0].Field());
+                OutputTable.Rows[1].SetField(i, 1);
+                OutputTable.Rows[2].SetField(i, 1);
+                OutputTable.Rows[3].SetField(i, 1);
+                OutputTable.Rows[4].SetField(i, 1);
+                OutputTable.Rows[5].SetField(i, 1);
+                OutputTable.Rows[6].SetField(i, 1);
+                OutputTable.Rows[7].SetField(i, 1);
+                OutputTable.Rows[8].SetField(i, 1);
+                OutputTable.Rows[9].SetField(i, 1);
+            }
+        }
+
+        private static int ConvertToTable(int day)
+        {
+            if (day > 7 && day < 15)
+                return 7;
+            if (day > 14 && day < 31)
+                return 8;
+            if (day > 30)
+                return 9;
+            else return day;
+        }
+
+        private static double getThresholdBlast(double yield)
+        {
+            if (yield <= 10)
+                return -170.68 * Math.Log(yield) + 689.47;
+            else
+                return -56.89 * Math.Log(yield) + 427.47;
+        }
+
         #endregion
+
 
         #region Output
 
@@ -155,7 +337,7 @@ namespace CBRN_Project.MVVM.Models.Engine.Nuclear
         {
             List<string> dailyDetails = new List<string>
                 { "New KIA (N)", "New DOW (CRN)", "Sum of New Fatalities", "New WIA (Nuclear)", "New CONV (Nuclear)", "New RTD",
-                    "Sum of Fatalities", "Sum of WIA", "Sum of Conv", "RTD"};
+                    "Sum of Fatalities", "Sum of WIA", "Sum of Conv", "Sum of RTD"};
             OutputTable = Radiological.RadiologicalAgent.InitTable(dailyDetails);
         }
         #endregion
